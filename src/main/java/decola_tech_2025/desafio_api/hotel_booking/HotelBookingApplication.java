f70d38f2201bcffe0198ec348f16c160574832ae
Soleane Mota <sem.sol@gmail.com>
2025-03-13 10:08:21 -0400

    Initial commit

---
+++ src/main/java/decola_tech_2025/desafio_api/hotel_booking/HotelBookingApplication.java
@@ -0,0 +1,13 @@
+package decola_tech_2025.desafio_api.hotel_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/decola_tech_2025/desafio_api/hotel_booking/HotelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package decola_tech_2025.desafio_api.hotel_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
