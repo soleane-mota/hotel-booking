b21263d594f32e531990d107186bba80193e0f3c
Soleane Mota <sem.sol@gmail.com>
2025-03-16 11:33:23 -0400

    fix: default value of role in User entity

--- src/main/java/decola_tech_2025/desafio_api/hotel_booking/model/entity/User.java
+++ src/main/java/decola_tech_2025/desafio_api/hotel_booking/model/entity/User.java
@@ -2,9 +2,12 @@ package decola_tech_2025.desafio_api.hotel_booking.model.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,6 +27,14 @@ public class User {
   @Column(unique = true)
   private String email;
   private String password;
+  @Enumerated(EnumType.STRING)
   private Role role = Role.USER;
+
+  @PrePersist
+  public void setDefaultRole() {
+    if (this.role == null) {
+      this.role = Role.USER;
+    }
+  }
 }
 
